b286fa72f03230129c34e063f1da9aa3664f3fa3
mooka007 <mooka3084@gmail.com>
2023-08-10 06:30:24 -0700

    airbnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
 
+from datetime import datetime
+import json
+from uuid import uuid4
+import models
+
+
+class Basemodel:
+    """ Class defines all attributes for other classes """
+
+    def __init__(self, *args, **kwards):
+        """ Initialization of Class """
+
+        if kwargs:
+            for keys, values in kwargs.items():
+                if keys != "__class__":
+                    if keys == "created_at" or keys == "updated_at":
+                        values = datetime.strptime(values, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, keys, values)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            models.storage.new(self)
+    def __str__(self):
+        """ Presentation of string """
+        return ("[{}] ({}) {}".format(self.__class__.__name__,self.id, self.__dict__))
+
