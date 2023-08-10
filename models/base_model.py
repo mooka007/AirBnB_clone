8ef7865ec0b8e819bba4afd96524b84aa8e4d983
mooka007 <mooka3084@gmail.com>
2023-08-10 10:07:15 -0700

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,21 @@ class Basemodel:
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
             models.storage.new(self)
+    
     def __str__(self):
         """ Presentation of string """
         return ("[{}] ({}) {}".format(self.__class__.__name__,self.id, self.__dict__))
+
+    def save(self):
+        """ Updates the public instance attribute with the current date and time """
+        self.update_at = datetime.today()
+        models.storage.save()
+    
+    def to_dict(self):
+        """ Returns a dictionary that includes all the keys and their corresponding values of the instance """
+        new = self.__dict__.copy()
+        new["created_at"] = self.created_at.isoformat()
+        new["updated_at"] = self.updated_at.isoformat()
+        new["__class__"] = self.__class__.__name__
+        return new 
 
