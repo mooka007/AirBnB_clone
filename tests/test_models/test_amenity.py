d8069cc911445b09ca777cd2ba253623e177767e
Abdesslam Mekouar <a.mekouar@outlook.com>
2023-08-13 12:33:17 -0700

    pycodestyle

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,42 +1,27 @@
 #!/usr/bin/python3
-"""test the Amenity method"""
+""" unit test for Amenity """
 import unittest
-from models.base_model import BaseModel
 from models.amenity import Amenity
-import pep8
-import inspect
+from datetime import datetime
 
 
-class Test_pep8(unittest.TestCase):
-    """pep8 test cases class"""
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/amenity.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+class CityTestCase(unittest.TestCase):
+    """ class for amenity test """
+
+    def test_amenity(self):
+        """existince"""
+        new = Amenity()
+        self.assertTrue(hasattr(new, "id"))
+        self.assertTrue(hasattr(new, "created_at"))
+        self.assertTrue(hasattr(new, "updated_at"))
+        self.assertTrue(hasattr(new, "name"))
+
+        """type test"""
+        self.assertIsInstance(new.id, str)
+        self.assertIsInstance(new.created_at, datetime)
+        self.assertIsInstance(new.updated_at, datetime)
+        self.assertIsInstance(new.name, str)
 
 
-class TestDocs(unittest.TestCase):
-    """Base model document tests"""
-
-    @classmethod
-    def setUpClass(cls):
-        """Testing class"""
-        cls.amenity_funcs = inspect.getmembers(Amenity, inspect.isfunction)
-
-    def test_module_docstring(self):
-        """module docstring length"""
-        self.assertTrue(len(Amenity.__doc__) >= 1)
-
-    def test_class_docstring(self):
-        """Class docstring length"""
-        self.assertTrue(len(Amenity.__doc__) >= 1)
-
-
-class TestAmenity(unittest.TestCase):
-    """Test the Amenity module"""
-    def test_class(self):
-        """Test the amenity class"""
-        self.assertEqual(Amenity.name, "")
-        self.assertTrue(issubclass(Amenity, BaseModel))
+if __name__ == '__main__':
+    unittest.main()
