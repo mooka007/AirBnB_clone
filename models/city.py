d54ed489832cfc977581d354c0f142f01d2c5c1f
Abdesslam Mekouar <a.mekouar@outlook.com>
2023-08-13 11:20:19 -0700

    pycodestyle

--- models/city.py
+++ models/city.py
@@ -4,14 +4,7 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-<<<<<<< HEAD
-        """Create the city module"""
-
-            state_id = ""
-                name = ""
-=======
     """Create the city module"""
 
     state_id = ""
     name = ""
->>>>>>> c74a74869b8f6fb38ed78263d6719dc8ff806557
