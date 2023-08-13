3cc0a1f757cc28fa75d1eda44f594e88398f5078
Abdesslam Mekouar <a.mekouar@outlook.com>
2023-08-13 11:19:15 -0700

    pycodestyle

--- models/amenity.py
+++ models/amenity.py
@@ -4,10 +4,5 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-<<<<<<< HEAD
-        """Amenity module"""
-            name = ""
-=======
     """Amenity module"""
     name = ""
->>>>>>> c74a74869b8f6fb38ed78263d6719dc8ff806557
